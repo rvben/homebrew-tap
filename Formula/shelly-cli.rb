f2c2f889062d6c469af8fd4aa132bee88493528a
Ruben J. Jongejan <ruben.jongejan@gmail.com>
2026-03-20 21:54:39 +0100

    feat(formula): update shelly-cli to v0.1.2

--- Formula/shelly-cli.rb
+++ Formula/shelly-cli.rb
@@ -1,26 +1,26 @@
 class ShellyCli < Formula
   desc "Fast CLI for discovering, monitoring, and controlling Shelly devices"
   homepage "https://github.com/rvben/shelly-cli"
-  version "0.1.1"
+  version "0.1.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/rvben/shelly-cli/releases/download/v#{version}/shelly-v#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "10bc6fb6b358a25537729485772c91d9bfd0aef94b970ed02a4d609f0ac781b1"
+      sha256 "1e61ffc78c7b63f1e2105df3064c04766a845140993074e9c23d13ff37d4c030"
     else
       url "https://github.com/rvben/shelly-cli/releases/download/v#{version}/shelly-v#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "422f377e183c321a183f61ddc0ee625fd0b513f3a8919e1c6dca296f6ecfb03d"
+      sha256 "59759cca3150df5d2f1b66db2ef9b13da97f27f4254103f29dc6acf5ead872db"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/rvben/shelly-cli/releases/download/v#{version}/shelly-v#{version}-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "68e41bbc5da57a9a2020001c4fce04c92b07e53178fe554379fed2e888f326d8"
+      sha256 "97d90adde9df3092f460e427229164835b451aa2a40bf1b8c72625f9e94114f8"
     else
       url "https://github.com/rvben/shelly-cli/releases/download/v#{version}/shelly-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "01cd33509f5f7669b875245e2a2f049a1239fe97790d451f36bfe76f7e5f1979"
+      sha256 "de171b11dbfd13ffb14f99e3be53d0f736270a006be7b1468f88045cc257ebde"
     end
   end
 
