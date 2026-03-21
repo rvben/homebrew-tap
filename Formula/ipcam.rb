d8cfdade16d526c9373016b3ca8a75ac283c8fe2
Ruben J. Jongejan <ruben.jongejan@gmail.com>
2026-03-21 07:52:57 +0100

    fix: add SHA256 checksums for v0.0.3 binaries

--- Formula/ipcam.rb
+++ Formula/ipcam.rb
@@ -7,16 +7,16 @@ class Ipcam < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/rvben/ipcam/releases/download/v#{version}/ipcam-macos-aarch64"
-      sha256 ""
+      sha256 "b0fb4e239cfe710ffa89e85739fa2fedd730a49e1c7582db21f6113cf1443c51"
     else
       url "https://github.com/rvben/ipcam/releases/download/v#{version}/ipcam-macos-x86_64"
-      sha256 ""
+      sha256 "eb7321cd6aff0b5dacceea80628981f88cf7f116e9255a9f78aa99ee2225a3e0"
     end
   end
 
   on_linux do
     url "https://github.com/rvben/ipcam/releases/download/v#{version}/ipcam-linux-x86_64"
-    sha256 ""
+    sha256 "ef98dadfb5c39426e5b2fd747e473d85bf54a90efd382094a53e5ac6cec5b451"
   end
 
   def install
