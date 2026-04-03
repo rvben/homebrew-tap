9e47e4784dd3c39c9e6f9afba2c4c76d1bb9be5e
Ruben J. Jongejan <ruben.jongejan@gmail.com>
2026-04-03 18:11:29 +0200

    Update clispec to v0.1.5

--- Formula/clispec.rb
+++ Formula/clispec.rb
@@ -1,26 +1,26 @@
 class Clispec < Formula
   desc "Score CLI tools against The CLI Spec"
   homepage "https://github.com/rvben/clispec-cli"
-  version "0.1.2"
+  version "0.1.5"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/rvben/clispec-cli/releases/download/v0.1.2/clispec-v0.1.2-aarch64-apple-darwin.tar.gz"
-      sha256 "022ffc5772e04ac40d5329b3cf0615002eace76af8e99e016b9c81a83e53ab1a"
+      url "https://github.com/rvben/clispec-cli/releases/download/v0.1.5/clispec-v0.1.5-aarch64-apple-darwin.tar.gz"
+      sha256 "6e5e408034391258ba72429db2dd1471f5f47e9cafd4e068dee7e459d7133f72"
     else
-      url "https://github.com/rvben/clispec-cli/releases/download/v0.1.2/clispec-v0.1.2-x86_64-apple-darwin.tar.gz"
-      sha256 "612be5689df62673b09216f558807eba19ea08dd86ae226374ad280629d913cd"
+      url "https://github.com/rvben/clispec-cli/releases/download/v0.1.5/clispec-v0.1.5-x86_64-apple-darwin.tar.gz"
+      sha256 "d4e8370a6d2710f2143a91ca275c3f70b4d06cdf84ccdad0972f5e8cf2b5326b"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/rvben/clispec-cli/releases/download/v0.1.2/clispec-v0.1.2-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "7039042cbb3941d5c18170ac43ad982e30e66ad668da50dd0e9df05cdee3ddfd"
+      url "https://github.com/rvben/clispec-cli/releases/download/v0.1.5/clispec-v0.1.5-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "0270004d55eff271815bdc0bb24395218d012ea5e74b871c6529b9f09019957e"
     else
-      url "https://github.com/rvben/clispec-cli/releases/download/v0.1.2/clispec-v0.1.2-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "1ddd6d22d3ebeb4f3cbccbbb5a4dc7bc58b774c71c39bc1d6a4eb58e291eadc6"
+      url "https://github.com/rvben/clispec-cli/releases/download/v0.1.5/clispec-v0.1.5-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "f84f7c0158e6ebb0970d827f869afe5e0083bd3e3973f2822dc3207d937ff1b4"
     end
   end
 
