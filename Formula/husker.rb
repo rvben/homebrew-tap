a99ce537166b0d923c776e659fbf80821e857feb
Ruben J. Jongejan <ruben.jongejan@gmail.com>
2026-06-10 19:25:32 +0200

    Update husker to v0.4.2

--- Formula/husker.rb
+++ Formula/husker.rb
@@ -1,26 +1,26 @@
 class Husker < Formula
   desc "MicroVM manager built on Firecracker (Linux) and Apple Virtualization.framework (macOS)"
   homepage "https://github.com/rvben/husker"
-  version "0.4.1"
+  version "0.4.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/rvben/husker/releases/download/v0.4.1/husker-v0.4.1-aarch64-apple-darwin.tar.gz"
-      sha256 "51bd11fbb91b899441738b5119e318d56e5a546a9447435b26798a6f6decc7f2"
+      url "https://github.com/rvben/husker/releases/download/v0.4.2/husker-v0.4.2-aarch64-apple-darwin.tar.gz"
+      sha256 "1e38dff2e36124373e747de86a6d220fc51bf7fb6c43cc0d97f97404b13d1d16"
     else
-      url "https://github.com/rvben/husker/releases/download/v0.4.1/husker-v0.4.1-x86_64-apple-darwin.tar.gz"
-      sha256 "c117d436661720dc9251604a1d0b12effdd5991169ed74c57ddd320dfaed773a"
+      url "https://github.com/rvben/husker/releases/download/v0.4.2/husker-v0.4.2-x86_64-apple-darwin.tar.gz"
+      sha256 "ec973c2ad64d1393a7153458e1529e9c4596e23d400389de4c4ba6dcd40070b2"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/rvben/husker/releases/download/v0.4.1/husker-v0.4.1-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "a013206666052817eedb057f7861f6fe9ac55dbe85a606a2742db1bd6390acb6"
+      url "https://github.com/rvben/husker/releases/download/v0.4.2/husker-v0.4.2-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "8ee9e7ee935179ad6905e22e5ec02a327f6ec250513f4e07fbe2ecd461dd09fd"
     else
-      url "https://github.com/rvben/husker/releases/download/v0.4.1/husker-v0.4.1-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "8260c0432969db2d4f5deea4ddaabfc4e61d8065fadcbf327ca3f7403aac6165"
+      url "https://github.com/rvben/husker/releases/download/v0.4.2/husker-v0.4.2-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "aae7ba4e5ff22888c153d187db7bb055d9ba5c06f0d6bcdfe97d82245a69476b"
     end
   end
 
