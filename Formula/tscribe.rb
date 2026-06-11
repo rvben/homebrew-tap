7dc4d1b9b28da2f058ad09d4c306d568d61fb1b3
Ruben J. Jongejan <ruben.jongejan@gmail.com>
2026-06-11 18:33:47 +0200

    Update tscribe to 0.2.1

--- Formula/tscribe.rb
+++ Formula/tscribe.rb
@@ -1,7 +1,7 @@
 class Tscribe < Formula
   desc "Transcribe any video/audio URL into agent-friendly markdown using whisper.cpp"
   homepage "https://github.com/rvben/tscribe"
-  version "0.2.0"
+  version "0.2.1"
   license "MIT"
 
   depends_on "ffmpeg"
@@ -9,21 +9,21 @@ class Tscribe < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/rvben/tscribe/releases/download/v0.2.0/tscribe-aarch64-apple-darwin.tar.gz"
-      sha256 "3cdf231a9880d3ba838f89bc4506897b85ad13699562e0ce2d5f67fae8d5fb9c"
+      url "https://github.com/rvben/tscribe/releases/download/v0.2.1/tscribe-aarch64-apple-darwin.tar.gz"
+      sha256 "70300b507a8c1b830e3c5a5a47c52ec25163c15b3faca2e8dd2dba7aff98b7e2"
     else
-      url "https://github.com/rvben/tscribe/releases/download/v0.2.0/tscribe-x86_64-apple-darwin.tar.gz"
-      sha256 "94abe6df0f7e696d49e058467c6e7f008d8ad2b0428d3ea2158b9c358b477a38"
+      url "https://github.com/rvben/tscribe/releases/download/v0.2.1/tscribe-x86_64-apple-darwin.tar.gz"
+      sha256 "b4b59d6223e02e3122052ed726faa752498498bf015ae1b0f0397fdf8fff1e33"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/rvben/tscribe/releases/download/v0.2.0/tscribe-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "87dabc68f7a11d99d744e9244ea54c57dcc635023e8798e44e260faf9ac11039"
+      url "https://github.com/rvben/tscribe/releases/download/v0.2.1/tscribe-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "1dd222948cdaa79d1f3390a49f7039ac3c38980e7bedbdc192bd43601e185b11"
     else
-      url "https://github.com/rvben/tscribe/releases/download/v0.2.0/tscribe-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "41aeba76048976cc84a6efec082af6c2474e75945fd6e04cef840a97d0503c6c"
+      url "https://github.com/rvben/tscribe/releases/download/v0.2.1/tscribe-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "bdc85bfd3fc9093ca0364740fcdea38605c2f16f0af691201c2a3fdc6710114d"
     end
   end
 
