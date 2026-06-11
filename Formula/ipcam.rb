544971d5bb8f06b4018c73f7b4e7ca73e6937838
Ruben J. Jongejan <ruben.jongejan@gmail.com>
2026-06-11 14:31:13 +0200

    Update ipcam to 0.0.7

--- Formula/ipcam.rb
+++ Formula/ipcam.rb
@@ -1,22 +1,22 @@
 class Ipcam < Formula
   desc "A CLI for managing IP cameras (Tapo, Reolink) via RTSP and vendor APIs"
   homepage "https://github.com/rvben/ipcam"
-  version "0.0.6"
+  version "0.0.7"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/rvben/ipcam/releases/download/v#{version}/ipcam-macos-aarch64"
-      sha256 "1e91e8810d27d3f8635bcf16377858a89bf2e2e6e4e71047188728b9e974dca0"
+      sha256 "98217ef5c24cba23746c31ea2cc761c4e4095af9b3981dd49dd7e6bedaa0e982"
     else
       url "https://github.com/rvben/ipcam/releases/download/v#{version}/ipcam-macos-x86_64"
-      sha256 "e2b246448f7aa89daa0830ddf6d95c1e4f88bb33daa125bcda7608ec816c5359"
+      sha256 "4528b858fee8ab49369001c76c5e950bdd43d024235a1028ce57c20de8f43a44"
     end
   end
 
   on_linux do
     url "https://github.com/rvben/ipcam/releases/download/v#{version}/ipcam-linux-x86_64"
-    sha256 "4dca266f0acf025caf6f6e342f8f244155147e8ea81171db5a735adabd26ed06"
+    sha256 "88298a314c01b688f4be186f9959538ab147345c38a7dd2db7dd19f61e01503e"
   end
 
   def install
