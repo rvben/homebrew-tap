41db7da15679cc2331ce36bb67148829ffe3a9b9
Ruben J. Jongejan <ruben.jongejan@gmail.com>
2026-06-11 14:37:02 +0200

    Update zoom-cli to 0.2.5

--- Formula/zoom-cli.rb
+++ Formula/zoom-cli.rb
@@ -1,26 +1,26 @@
 class ZoomCli < Formula
   desc "CLI for Zoom"
   homepage "https://github.com/rvben/zoom-cli"
-  version "0.2.4"
+  version "0.2.5"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/rvben/zoom-cli/releases/download/v0.2.4/zoom-cli-v0.2.4-aarch64-apple-darwin.tar.gz"
-      sha256 "0518c0b5d8bbf8c69745798156ae19cf06721538804bb190398674a30c98e623"
+      url "https://github.com/rvben/zoom-cli/releases/download/v0.2.5/zoom-cli-v0.2.5-aarch64-apple-darwin.tar.gz"
+      sha256 "d750af239f716263650d5525763b10e9a46be0aff9159d8e6c330280df562fbc"
     else
-      url "https://github.com/rvben/zoom-cli/releases/download/v0.2.4/zoom-cli-v0.2.4-x86_64-apple-darwin.tar.gz"
-      sha256 "bb5e1ae4de7016753fd0a134aa8cd435c0707ee92717ebc2fa5a83263d0744aa"
+      url "https://github.com/rvben/zoom-cli/releases/download/v0.2.5/zoom-cli-v0.2.5-x86_64-apple-darwin.tar.gz"
+      sha256 "06d7fa9a11c055b125f6968a3ac9fdb995fb8eb002c07dff5ca5b6c973e573a8"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/rvben/zoom-cli/releases/download/v0.2.4/zoom-cli-v0.2.4-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "8a8e88143d02d85e4abe56f6a65839f0b896189d230589e59ea92f35eb0dc6b1"
+      url "https://github.com/rvben/zoom-cli/releases/download/v0.2.5/zoom-cli-v0.2.5-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "64bf4528af45e16ed61dd96123da85caf16475cd996f5db75fd19630ac604782"
     else
-      url "https://github.com/rvben/zoom-cli/releases/download/v0.2.4/zoom-cli-v0.2.4-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "4c3bd7f0429d062e6f4389fc219274f542fdc778e6be9232c0b3cfc3fa4e9e2c"
+      url "https://github.com/rvben/zoom-cli/releases/download/v0.2.5/zoom-cli-v0.2.5-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "3f2982248eef97a55841fd7eb6bcc31c3e446a8d699717979983bf63ce97a771"
     end
   end
 
