9dc3ef7d69221d9097b942ceb0c669041e238e12
Ruben J. Jongejan <ruben.jongejan@gmail.com>
2026-07-03 10:16:38 +0200

    esprite 0.4.0

--- Formula/esprite.rb
+++ Formula/esprite.rb
@@ -1,17 +1,17 @@
 class Esprite < Formula
   desc "ESP32/Arduino firmware simulator with an agent-facing CLI (host-native + QEMU backends)"
   homepage "https://github.com/rvben/esprite"
-  version "0.3.0"
+  version "0.4.0"
   license "MIT"
 
   on_macos do
-    url "https://github.com/rvben/esprite/releases/download/v0.3.0/esprite-0.3.0-darwin-arm64.tar.gz"
-    sha256 "7bdeccdee534d0b25db0ea1487c20661c8164e236246e81566d8b812e2d67cad"
+    url "https://github.com/rvben/esprite/releases/download/v0.4.0/esprite-0.4.0-darwin-arm64.tar.gz"
+    sha256 "44c344ccf2a3badf6717c54ea582e1f309a3de1df0c966ece7c143aa4d52a515"
   end
 
   on_linux do
-    url "https://github.com/rvben/esprite/releases/download/v0.3.0/esprite-0.3.0-linux-x86_64.tar.gz"
-    sha256 "30c23cefc85967fc71a14398e195608efaeb7f2eba48abaa1eb880dd0ce73eae"
+    url "https://github.com/rvben/esprite/releases/download/v0.4.0/esprite-0.4.0-linux-x86_64.tar.gz"
+    sha256 "e063f7beb1be4d8f8bd63314f35d2d50fbebba2e9f9b6fe097d03d58ec05b922"
   end
 
   def install
