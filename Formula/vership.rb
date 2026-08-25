03ed1dce3b7fa0398794d08cae9ca27a262e8505
Ruben J. Jongejan <ruben.jongejan@gmail.com>
2026-08-25 21:19:43 +0200

    Update vership to v0.5.20

--- Formula/vership.rb
+++ Formula/vership.rb
@@ -1,26 +1,26 @@
 class Vership < Formula
   desc "Multi-target release orchestrator"
   homepage "https://github.com/rvben/vership"
-  version "0.5.19"
+  version "0.5.20"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/rvben/vership/releases/download/v0.5.19/vership-v0.5.19-aarch64-apple-darwin.tar.gz"
-      sha256 "5cf8e1bbaf1653fd77f53babc699a5223014baff8ec12b3aff23529ed1447d8f"
+      url "https://github.com/rvben/vership/releases/download/v0.5.20/vership-v0.5.20-aarch64-apple-darwin.tar.gz"
+      sha256 "6782c301b4a20c6343c556888a316d83e9a1ccc93fd6847484ab9e4df5fb725a"
     else
-      url "https://github.com/rvben/vership/releases/download/v0.5.19/vership-v0.5.19-x86_64-apple-darwin.tar.gz"
-      sha256 "8ba9c40c68c8894635934a5fc9b2380be800a3e572e088b0930fee3581e14141"
+      url "https://github.com/rvben/vership/releases/download/v0.5.20/vership-v0.5.20-x86_64-apple-darwin.tar.gz"
+      sha256 "3554e2cbcba3d090d3f3142be6262a34c6c27f1a73f3606885e6f023e72a1b05"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/rvben/vership/releases/download/v0.5.19/vership-v0.5.19-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "91699a678244aab9ba68c8b2cc885422a3c30d8069fe2c1099a6ea6d5723170b"
+      url "https://github.com/rvben/vership/releases/download/v0.5.20/vership-v0.5.20-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "ae46bd338a0122a09316d325a0cf18daa068af6c27d6f929c733a1407b7cd335"
     else
-      url "https://github.com/rvben/vership/releases/download/v0.5.19/vership-v0.5.19-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "b18519ac17aa0d95de6c33fb1713020c099b0f81acf5e63a8c502c614d1c5a93"
+      url "https://github.com/rvben/vership/releases/download/v0.5.20/vership-v0.5.20-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "9d30be30d64ad92af1f60a8b9e1cb36d15cce92483758290c533f62a08c4b7ff"
     end
   end
 
