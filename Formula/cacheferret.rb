d9afa3f1ecd60aa3972f249bba9f74be99839d7d
Ruben J. Jongejan <ruben.jongejan@gmail.com>
2026-08-26 08:39:45 +0200

    chore(homebrew): update cacheferret to v0.4.1

--- Formula/cacheferret.rb
+++ Formula/cacheferret.rb
@@ -1,26 +1,26 @@
 class Cacheferret < Formula
   desc "Find and safely clean developer caches across macOS and Linux"
   homepage "https://github.com/rvben/cacheferret"
-  version "0.3.1"
+  version "0.4.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/rvben/cacheferret/releases/download/v0.3.1/cacheferret-v0.3.1-aarch64-apple-darwin.tar.gz"
-      sha256 "339b5a9f0e01cbe5b63a3735027ae49f764993ca2e75a54c07050443d9f1c503"
+      url "https://github.com/rvben/cacheferret/releases/download/v0.4.1/cacheferret-v0.4.1-aarch64-apple-darwin.tar.gz"
+      sha256 "cd3f03925f1d36814e172c903754771d4d216670fdb78b9831e5e44b56e7157b"
     else
-      url "https://github.com/rvben/cacheferret/releases/download/v0.3.1/cacheferret-v0.3.1-x86_64-apple-darwin.tar.gz"
-      sha256 "536a411deb84bbd3614cbc39cf435d0566e659ca736661e37b1db15cdffe2da7"
+      url "https://github.com/rvben/cacheferret/releases/download/v0.4.1/cacheferret-v0.4.1-x86_64-apple-darwin.tar.gz"
+      sha256 "1d30e2173e2f090a9dc6296c0550e455a629de5f8d3f9aab34a1bf59b685143b"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/rvben/cacheferret/releases/download/v0.3.1/cacheferret-v0.3.1-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "28b752d05b767e59ec8968a41d10539573718f4d62d0e540565e56c5a8d96dda"
+      url "https://github.com/rvben/cacheferret/releases/download/v0.4.1/cacheferret-v0.4.1-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "3520c0993b69408445ea5ffb4526e6e8c202f22d978d8d28894a27ba2a8c171b"
     else
-      url "https://github.com/rvben/cacheferret/releases/download/v0.3.1/cacheferret-v0.3.1-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "b5399b450ab8e8fe3cd26c82afb25a64a0c460873a1ad07a41154cc40bb59dbe"
+      url "https://github.com/rvben/cacheferret/releases/download/v0.4.1/cacheferret-v0.4.1-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "acb21bc99b46cf49dd0924b8a4f233ab5ef9164fdb3e4060c786c163434b3020"
     end
   end
 
