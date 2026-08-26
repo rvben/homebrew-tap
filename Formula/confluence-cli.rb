d2a22012deb604cd306fc8aff237ba05019bb02f
Ruben J. Jongejan <ruben.jongejan@gmail.com>
2026-08-26 10:49:29 +0200

    fix(confluence-cli): avoid aborting tap loads

--- Formula/confluence-cli.rb
+++ Formula/confluence-cli.rb
@@ -4,22 +4,22 @@ class ConfluenceCli < Formula
   license "MIT"
 
   on_macos do
-    if Hardware::CPU.arm?
+    on_arm do
       url "https://github.com/rvben/confluence-cli/releases/download/v0.1.18/confluence-cli-v0.1.18-aarch64-apple-darwin.tar.gz"
       sha256 "f66b40e33587f402af86f78c77b3fd63af97c1fd05b94cd4fe5b47b057f80511"
-    else
+    end
+
+    on_intel do
       url "https://github.com/rvben/confluence-cli/releases/download/v0.1.18/confluence-cli-v0.1.18-x86_64-apple-darwin.tar.gz"
       sha256 "cdaa8e86d8f610729e0448d9d011f7b1996caab0a85366f9ec23a694fcf0a053"
     end
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/rvben/confluence-cli/releases/download/v0.1.18/confluence-cli-v0.1.18-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "a900f70e33c7794765ef54ed610c87ac92d311a29a10a57bfd167592093c1850"
-    else
-      odie "Linux ARM is not supported by this formula."
-    end
+    depends_on arch: :x86_64
+
+    url "https://github.com/rvben/confluence-cli/releases/download/v0.1.18/confluence-cli-v0.1.18-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "a900f70e33c7794765ef54ed610c87ac92d311a29a10a57bfd167592093c1850"
   end
 
   def install
