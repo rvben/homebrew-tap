7f1bb7b0a0a3224aedc0d589ab5bf6006465a06f
Ruben J. Jongejan <ruben.jongejan@gmail.com>
2026-08-26 10:16:39 +0200

    fix(cacheferret): support ARM Linux installs

--- Formula/cacheferret.rb
+++ Formula/cacheferret.rb
@@ -5,20 +5,24 @@ class Cacheferret < Formula
   license "MIT"
 
   on_macos do
-    if Hardware::CPU.arm?
+    on_arm do
       url "https://github.com/rvben/cacheferret/releases/download/v0.4.2/cacheferret-v0.4.2-aarch64-apple-darwin.tar.gz"
       sha256 "c8c44756f43683f6135465436ce677d607efe85b90c6784a2702422cab221336"
-    else
+    end
+
+    on_intel do
       url "https://github.com/rvben/cacheferret/releases/download/v0.4.2/cacheferret-v0.4.2-x86_64-apple-darwin.tar.gz"
       sha256 "287b3545676edca7cc1f5fd3a498030b03a3e37e87a9437c08fe2af69904ff44"
     end
   end
 
   on_linux do
-    if Hardware::CPU.arm?
+    on_arm do
       url "https://github.com/rvben/cacheferret/releases/download/v0.4.2/cacheferret-v0.4.2-aarch64-unknown-linux-gnu.tar.gz"
       sha256 "f1e34e7a003925be980a6d2755783d0b88ace6cb44bd919f7564404e9884878b"
-    else
+    end
+
+    on_intel do
       url "https://github.com/rvben/cacheferret/releases/download/v0.4.2/cacheferret-v0.4.2-x86_64-unknown-linux-gnu.tar.gz"
       sha256 "df6d251607859bd1f2895c0a2ce8bd8ead2cdedfcdfecc185ff3ec7348fd3e38"
     end
